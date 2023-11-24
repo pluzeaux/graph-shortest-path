47bba90fd3027d69fbfcebd855e2507346a04805
pluzeaux <philippe.luzeaux@gmail.com>
2023-11-24 10:56:34 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+struct Graph {
+
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ * 
+ * Your Graph object will be instantiated and called as such:
+ * let obj = Graph::new(n, edges);
+ * obj.add_edge(edge);
+ * let ret_2: i32 = obj.shortest_path(node1, node2);
+ */
+impl Graph {
+
+    fn new(n: i32, edges: Vec<Vec<i32>>) -> Self {
+       Graph {} 
+    }
+    
+    fn add_edge(&self, edge: Vec<i32>) {
+        
+    }
+    
+    fn shortest_path(&self, node1: i32, node2: i32) -> i32 {
+       0 
+    }
+}

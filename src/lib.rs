af80a91bd440d9562fbf283e68a9b21bd06515a0
pluzeaux <philippe.luzeaux@gmail.com>
2023-11-27 12:13:48 +0100

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,57 @@
-struct Graph {
+use std::{collections::HashMap, hash::Hash};
 
+#[derive(Copy, Clone, Debug, Default, PartialEq, PartialOrd, Eq, Ord, Hash)]
+struct Node<T> {
+    value: T 
 }
 
+#[derive(Copy, Clone, Debug, Default, PartialEq, PartialOrd, Eq, Ord, Hash)]
+struct Edge<T> {
+   outbound: Node<T>,
+   inbound: Node<T>,
+   weight: i32,
+}
 
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- * 
- * Your Graph object will be instantiated and called as such:
- * let obj = Graph::new(n, edges);
- * obj.add_edge(edge);
- * let ret_2: i32 = obj.shortest_path(node1, node2);
- */
-impl Graph {
+struct Graph<T> {
+   graph: HashMap<Node<T>, Edge<T>>,
+}
+ 
+// `&self` means the method takes an immutable reference.
+// If you need a mutable reference, change it to `&mut self` instead.
+// 
+// Your Graph object will be instantiated and called as such:
+// let obj = Graph::new(n, edges);
+// obj.add_edge(edge);
+// let ret_2: i32 = obj.shortest_path(node1, node2);
+impl<T: PartialEq + Eq + Hash + Copy> Graph<T> {
+    fn new(n: i32, edges: Vec<(T, T, i32)>) -> Self {
+      let mut g: HashMap<Node<T>, Vec<Edge<T>>> = HashMap::new();
 
-    fn new(n: i32, edges: Vec<Vec<i32>>) -> Self {
-       Graph {} 
+      for e in edges {
+         let edg = Edge {
+            outbound: Node { value: e.0 },
+            inbound: Node { value: e.1 },
+            weight: e.2,
+         };
+         g
+            .entry(edg.outbound)
+            .or_insert_with(Vec::new)
+            .insert(edg);
+      }
+
+      Graph {
+         graph: g
+      }
     }
     
-    fn add_edge(&self, edge: Vec<i32>) {
-        
+    fn add_edge(&mut self, edge: (T, T, i32)) {
+         let edg = Edge {
+            outbound: Node { value: edge.0 },
+            inbound: Node { value: edge.1 },
+            weight: edge.2,
+         };
+
+         self.graph.insert(edg.outbound, edg);
     }
     
     fn shortest_path(&self, node1: i32, node2: i32) -> i32 {
